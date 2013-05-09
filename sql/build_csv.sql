b15be2b260f67b0389f05dc67b2b2b412aa9fa0e
Nilesh Chakraborty <nilesh@nyoombl.com>
2013-05-10 01:50:55 +0530

    Added lib jars. Added comment on sql file.

--- sql/build_csv.sql
+++ sql/build_csv.sql
@@ -1,10 +1,12 @@
 -- Create a csv file with each line containing the pair: <item ID>, <propertyID>
+-- 7 is the relative score. This is hardcoded for now. More on this in docs.
 SELECT DISTINCT s_item,
                 s_property,
                 7 AS score INTO OUTFILE '/tmp/item-property.csv' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
 FROM statement;
 
 -- Create a csv file with each line containing the pair: <item ID>, <propertyID:value>
+-- Different relative scores for different properties - depending upon how popular the property is on wikidata.
 SELECT DISTINCT s_item,
                 (CONCAT(cast(s_property AS char), ':', cast((CASE WHEN s_item_value IS NULL THEN s_string_value ELSE s_item_value END) AS char))) AS property_value,
                 (CASE
